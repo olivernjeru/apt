3b075700e67169aa9cc1de6c0b5e4a1f6232d17a
Oliver Njiru <olivernjiru@gmail.com>
2022-11-09 08:54:48 +0300

    update README.md

---
+++ apt3040/NetBeansProjects/gratuitysystemtest/src/gratuitysystemtest/ContractEmployee.java
@@ -0,0 +1,5 @@
+package gratuitysystemtest;
+
+public class ContractEmployee extends Employee {
+    
+}
--- apt3040/NetBeansProjects/gratuitysystemtest/src/gratuitysystemtest/Gratuitysystemtest.java
+++ apt3040/NetBeansProjects/gratuitysystemtest/src/gratuitysystemtest/Gratuitysystemtest.java
@@ -5,6 +5,8 @@ public class Gratuitysystemtest {
     public static void main(String[] args) {
         Employee emobj=new Employee("Bob", "elll", 80000, 0);
         System.out.println("Name: " + emobj.getName() + " " + "Employee ID: " + emobj.getEmpid() + " " + "Salary: " + emobj.getSalary() + " " + "Gratuity: " + emobj.getGratuity());
+        Manager mobj= new Manager("Mandela", "e665", 400000, 20000, "Accounting", 20000);
+        System.out.println("Manager Name: " + mobj.getName() + " " + "Employee ID: " + mobj.getEmpid() + " " + "Salary: " + mobj.getSalary() + " " + "Gratuity: " + mobj.getGratuity() + " " + "Department: " + mobj.getDepartment() + " " + "Bonus: " + mobj.getBonus());
     }
     
 }
