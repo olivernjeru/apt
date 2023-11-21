72c32da71c2c5eef719f90b5b2ad168186e20da2
Oliver Njeru <olivernjiru@gmail.com>
2023-11-21 08:44:26 +0300

    add week 12

---
+++ ist4035/week12/db-project/index.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'oliver',
+    password: 'psswd123',
+    database: 'schooldb'
+});
+connection.connect();
+
+function viewStudents() {
+    let sql = "SELECT id, name FROM students";
+    connection.query(sql, function (err, rows, fields) {
+        if (err) throw err;
+        console.log('Records available: ' + rows.length);
+        for (let i = 0; i < rows.length; i++) {
+            console.log(rows[i].id + ' ' + rows[i].name);
+        }
+
+    });
+}
+viewStudents();
+// connection.end(); // NB: if running a server, remove this line
+
+function addStudent() {
+    let sql = "INSERT INTO students (id, name) VALUES " +
+    "(1, 'John Doe')";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log('Records inserted: ' + result.affectedRows);
+    });
+}
+
+addStudent();
+viewStudents();
+// connection.end(); //NB: if running a server, remove this line
