286750b6d6aa9e88fc2a60450ec3ce6449a42ed9
Oliver Njeru <olivernjiru@gmail.com>
2025-03-10 12:39:00 +0300

    add destructuringAssignment4

---
+++ app4035/week9/destructuringAssignment4.js
@@ -0,0 +1,7 @@
+const [firstName, doubleNum] = ["John", x => x * 2, true, 34];
+
+console.log(firstName);
+console.log(doubleNum(8));
+
+console.log(typeof firstName);
+console.log(typeof doubleNum);
