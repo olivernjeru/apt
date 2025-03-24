e1645fd51be5fa81b602726369146fd2fef68c56
Oliver Njeru <olivernjiru@gmail.com>
2025-03-24 11:40:42 +0300

    update About page to displaydata,  staff members with their titles

--- app4035/week10/routing-app/src/app/about/page.js
+++ app4035/week10/routing-app/src/app/about/page.js
@@ -1,9 +1,28 @@
+import React from 'react';
 
 export default function About() {
+    const data = [
+        {
+            id: 1,
+            name: 'John Doe',
+            title: 'Director'
+        },
+        {
+            id: 2,
+            name: 'Jane Doe',
+            title: 'Managing Director'
+        }
+    ];
+    const listItems = data.map((employee) =>
+        <li key={employee.id.toString()}>
+            {employee.name} | {employee.title}
+        </li>
+    );
     return (
         <div>
-            <h1>This is the about us page</h1>
-            <p>This is the about us page content.</p>
+            <h1>This is the About Page</h1>
+            <p>Below are our staff members:</p>
+            <ul>{listItems}</ul>
         </div>
     );
 }
