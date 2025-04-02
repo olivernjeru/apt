eb40f26b7aa236ac771ca162190204082ccf2892
Oliver Njeru <olivernjiru@gmail.com>
2025-04-02 11:46:57 +0300

    add contact form to Contact page with submit handling

--- app4035/week10/routing-app/src/app/contact/page.js
+++ app4035/week10/routing-app/src/app/contact/page.js
@@ -1,9 +1,22 @@
+"use client";
 
-export default function About() {
+export default function Contact() {
+    const handleSubmit = (e) => {
+        console.log('form submitted');
+        e.preventDefault();
+    }
+
     return (
         <div>
             <h1>This is the contact us page</h1>
             <p>This is the contact us page content.</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" />
+                </label>
+                <input type="submit" value="submit" />
+            </form>
         </div>
     );
 }
