018b56f6ab770554d9fe6dc2f94736c12cd1330e
Oliver Njeru <olivernjiru@gmail.com>
2025-04-02 11:43:02 +0300

    refactor services page to update content and add button functionality

--- app4035/week10/routing-app/src/app/services/page.js
+++ app4035/week10/routing-app/src/app/services/page.js
@@ -1,10 +1,31 @@
-import PopulationData from "../PopulationData";
+// import PopulationData from "../PopulationData";
 
-export default function About() {
+// export default function About() {
+//     return (
+//         <div>
+//             <h1>This is the services page. We offer Data Services such as:</h1>
+//             <PopulationData />
+//         </div>
+//     );
+// }
+
+"use client";
+
+export default function Services() {
+    function doSomethingOne(event) {
+        console.log('This is function 1');
+    }
+
+    const doSomethingTwo = (e) => {
+        console.log('This is function 2');
+    }
+
     return (
         <div>
-            <h1>This is the services page. We offer Data Services such as:</h1>
-            <PopulationData />
+            <h1>This is the services page</h1>
+            <p>This is teh services page content</p>
+            <button onClick={doSomethingOne}>Button 1</button>
+            <button onClick={doSomethingTwo}>Button 2</button>
         </div>
     );
 }
